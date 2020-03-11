a93e16d747281888199ce7e9d7a75187ee37d3de
Eladio <eladiojuniorrodriguez@gmail.com>
2020-03-11 09:47:02 -0400

    Initial Commit

---
+++ Stack/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduzca la oracion gramatical original que desee invertir: ");
+            string palabra = Console.ReadLine();
+            string[] separar = palabra.Split(' ');
+            Stack<string> ordenPila = new Stack<string>(separar);
+            Console.WriteLine("");
+
+            Console.Write("La oracion gramatical invertida es: ");
+
+            for (int i = 0; i < separar.Length; i++)
+            {
+                Console.Write($"{ordenPila.Pop()} ");
+            }
+            Console.WriteLine("");
+        }
+    }
+}
